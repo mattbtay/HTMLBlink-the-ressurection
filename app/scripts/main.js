083e0e910bf8c19cedea975f770b81807b613604
Matt Taylor <mattbtay@gmail.com>
2015-03-10 23:11:53 -0500

    clean up from yeoman silliness and elements
    got rid of the yeoman silly stuff, and created a basic form for the
    text.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+// take field value and move it to new section - placeholder for real js magic
+function getText(){
+
+	var blinkingText = $('#blinkText').val();
+
+	$('.generated-blink').text(blinkingText);
+
+	
+}
+
+//reset the text field and clear the created text - placeholder
+function resetText(){
+
+	$('#blinkText').val('');
+
+	$('.generated-blink').text('');
+
+	
+}
+
+$(document).ready(function(){
+
+
+	$('#blinkSubmit').click(function(){
+		getText();
+	});
+
+	$('#blinkReset').click(function(){
+		resetText();
+	});
+
+});
