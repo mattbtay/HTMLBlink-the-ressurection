a7e9166d8b9a5bfd089298ca226f24ceca3bba84
Matt Taylor <mattbtay@gmail.com>
2015-03-14 12:23:21 -0500

    updates to js and css
    added blinking text and some style changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,34 @@
 'use strict';
 
+function blink(HTMLblinkTimer) {
+
+	setInterval(function(){
+		$('blink').toggleClass('HTMLblink');
+	}, HTMLblinkTimer);
+
+	$('#HTMLblinkReset').click(function(){
+		clearInterval();
+	});
+	
+}
+
 // take field value and move it to new section - placeholder for real js magic
 function getText(){
+	
+	var HTMLblinkTime = $('#HTMLblinkTime').val();
+	console.log(HTMLblinkTime);
 
-	var blinkingText = $('#blinkText').val();
+	var HTMLblinkingText = $('#HTMLblinkText').val();
+	console.log(HTMLblinkingText);
 
-	$('.generated-blink').text(blinkingText);
+	$('.generated-blink').html('<blink>' + HTMLblinkingText + '</blink>');
 
 	$('.panel').fadeIn();
+
+	blink(HTMLblinkTime);
+	setTimeout(function(){
+		$('#HTMLblinkText', '#HTMLBlinkTime', '#HTMLblinkSubmit').addClass('disabled');
+	},500);
 
 	
 }
@@ -15,23 +36,28 @@ function getText(){
 //reset the text field and clear the created text - placeholder
 function resetText(){
 
-	$('#blinkText').val('');
-
+	$('#HTMLblinkText').val('');
+	$('#HTMLblinkTime').val('');
+	$('.generated-blink').text('');
 
 	//$('.generated-blink').text('');
 
 	$('.panel').fadeOut();
 }
 
+
+
 $(document).ready(function(){
 
 
-	$('#blinkSubmit').click(function(){
+	$('#HTMLblinkSubmit').click(function(){
+		console.log('clicked');
 		getText();
 	});
 
-	$('#blinkReset').click(function(){
+	$('#HTMLblinkReset').click(function(){
 		resetText();
+		console.log('clicked 3');
 	});
 
 });
